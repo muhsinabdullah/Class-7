e202c58c1db221609687414cc0fecdef29f04425
muhsinabdullah <muhsinabdullah1250@gmail.com>
2022-10-26 09:30:52 +0600

    js changed

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const add =(value1,value2)=>{
 
 let recievedResult = add(50,50);
     console.log(recievedResult);
+
+definationHoisted();
+
+definationNotHoisted();
+
+function definationHoisted(){
+    console.log("I am hoisted");
+};
+
+var definationNotHoisted = function(){
+    console.log("I am not hoisted");
+};
